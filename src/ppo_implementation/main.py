6180e2d5a518bb25f48b6bdd32dc9349f447d734
TabitoSaito <domi2601.hoelzl@gmail.com>
2025-12-27 14:22:42 +0100

    Load hyperparameters from file

--- src/ppo_implementation/main.py
+++ src/ppo_implementation/main.py
@@ -1,24 +1,25 @@
 from ppo_implementation.train.train import train_loop
-from ppo_implementation.agents.ppo_agent import PPOAgent
-from ppo_implementation.networks.networks import PolicyNet, ValueNet
-from ppo_implementation.buffers.buffer import RolloutBuffer
 from ppo_implementation.train.evaluate import eval_agent
+from ppo_implementation.utils.helper import build_agent
+import yaml
 import gymnasium as gym
 import datetime
+import os
 
-env = gym.make("CartPole-v1")
+env = gym.make("CartPole-v1", render_mode="rgb_array")
 
 obs_dim = env.observation_space.shape[0]
 act_dim = env.action_space.n
 
-policy = PolicyNet(obs_dim, act_dim)
-value = ValueNet(obs_dim)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+config = "config"
+file = "default.yaml"
+with open(os.path.join(current_dir, config, file)) as stream:
+        config = yaml.safe_load(stream)
 
-buffer = RolloutBuffer(64 * 32, obs_dim)
+agent = build_agent(obs_dim, act_dim, config)
 
-agent = PPOAgent(policy, value, buffer)
-
-train_loop(agent, env, episodes=1, override=True, storage=datetime.datetime.now().strftime("%d%m%y%H%M%S"))
+train_loop(agent, env, episodes=20, override=True, storage=datetime.datetime.now().strftime("%d-%m-%y-%H-%M-%S"), video_interval=5)
 
 print("eval")
 print(eval_agent(agent, env))
