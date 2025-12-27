638e6b3f1ab059ae1923fdbd77b5e8566dba5c0f
TabitoSaito <domi2601.hoelzl@gmail.com>
2025-12-27 14:25:16 +0100

    Load hyperparameters from file

--- src/ppo_implementation/agents/ppo_agent.py
+++ src/ppo_implementation/agents/ppo_agent.py
@@ -3,18 +3,17 @@ import torch
 
 
 class PPOAgent:
-    def __init__(self, policy_net, value_net, buffer, update_episodes=10, clip_eps=0.2) -> None:
+    def __init__(self, policy_net, value_net, buffer, config) -> None:
         self.policy_net = policy_net
         self.value_net = value_net
 
         self.optimizer = optim.Adam(
             list(self.policy_net.parameters()) + list(self.value_net.parameters()),
-            lr=3e-4,
+            lr=config["LR"],
         )
 
+        self.config = config
         self.buffer = buffer
-        self.update_episodes = update_episodes
-        self.clip_eps = clip_eps
 
     def act(self, state):
         dist = self.policy_net(state)
@@ -27,12 +26,12 @@ class PPOAgent:
     def remember(self, state, action, reward, done, log_prob, value):
         self.buffer.add(state, action, reward, done, log_prob.detach(), value.detach())
 
-    def update(self, batch_size):
+    def update(self):
         self.buffer.compute_gae()
-        for _ in range(self.update_episodes):
+        for _ in range(self.config["UPDATE_EPISODES"]):
             idx = torch.randperm(self.buffer.size)
-            for i in range(0, self.buffer.size, batch_size):
-                b = idx[i : i + batch_size]
+            for i in range(0, self.buffer.size, self.config["BATCH_SIZE"]):
+                b = idx[i : i + self.config["BATCH_SIZE"]]
 
                 dist = self.policy_net(self.buffer.obs[b])
                 new_logp = dist.log_prob(self.buffer.actions[b])
@@ -41,14 +40,14 @@ class PPOAgent:
                 loss_pi = -torch.mean(
                     torch.min(
                         ratio * self.buffer.advantages[b],
-                        torch.clamp(ratio, 1 - self.clip_eps, 1 + self.clip_eps)
+                        torch.clamp(ratio, 1 - self.config["CLIP"], 1 + self.config["CLIP"])
                         * self.buffer.advantages[b],
                     )
                 )
 
                 loss_v = torch.mean((self.value_net(self.buffer.obs[b]) - self.buffer.returns[b]) ** 2)
 
-                loss = loss_pi + 0.5 * loss_v - 0.01 * dist.entropy().mean()
+                loss = loss_pi + self.config["VALUE_DISCOUNT"] * loss_v - self.config["ENTROPY_COEF"] * dist.entropy().mean()
 
                 self.optimizer.zero_grad()
                 loss.backward()
